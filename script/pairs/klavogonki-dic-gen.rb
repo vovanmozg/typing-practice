b3287618e4c564bea6ceb55da6afa20713781dbc
vovanmozg <vovanmozg@gmail.com>
2024-02-25 17:12:08 +0100

    Add dictionary

--- script/pairs/klavogonki-dic-gen.rb
+++ script/pairs/klavogonki-dic-gen.rb
@@ -1,8 +1,19 @@
+output_dir = File.join(File.expand_path('..', File.expand_path('..', __dir__)), 'dictionary', 'pairs')
+
 symbols = %{!1{2}3&4-5%6<7>8(9)0_^=+[@]#*|:;'",\\.|/?}
-File.write 'klavogonki.txt', symbols
+
+File.write "#{output_dir}/pairs.txt", symbols
   .split('')
   .permutation(2)
   .to_a
   .map{|a, b| "#{a}#{b}"}
   .join(' ')
+
+
+File.write "#{output_dir}/pairs-phrases.txt", symbols
+                               .split('')
+                               .permutation(2)
+                               .to_a
+                               .map{|a, b| "#{a}#{b} " * 20}
+                               .join("\n")
 
